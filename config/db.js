d0e9338e664f3f393d980b82fa6579044badc6b7
Mashud Ahmed <mashudahmed1980@gmail.com>
2019-11-14 21:47:24 -0500

    Updated contact model

---
+++ config/db.js
@@ -0,0 +1,6 @@
+// Module is a container, object container. Each module/file and node by default is 2 surley braces
+module.exports = {
+  //KEy value pari: URI is property of db.js[db.js is a module coz we put module exports inside the curly braces is one property that means
+  // we exposed the URI property to other parts of my code. ]
+  URI: "mongodb://localhost/test"
+};
